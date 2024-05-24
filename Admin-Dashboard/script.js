9e31ebb2a56b5b4d2296ed74b7d346a61abeae8b
Phong <PhongTran3@my.unt.edu>
2024-05-23 21:42:23 -0500

    Styled project card and added footer

--- Admin-Dashboard/script.js
+++ Admin-Dashboard/script.js
@@ -2,6 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const headerDashboard = document.getElementById("header-dashboard");
   const navOverlay = document.getElementById("nav-overlay");
   const navbarContainer = document.getElementById("nav-container");
+  const footerNavItems = document.querySelectorAll(".footer-nav-mobile .nav-mobile-menu li");
+
+  const containerMap = {
+    announcement: document.getElementById("announcements-container"),
+    home: document.getElementById("projects-container"),
+    trending: document.getElementById("trending-container"),
+  };
 
   //Clicking the dashboard logo on the header will open the navbar overlay.
   headerDashboard.addEventListener("click", () => {
@@ -13,5 +20,20 @@ document.addEventListener("DOMContentLoaded", () => {
   navOverlay.addEventListener("click", () => {
     navOverlay.style.display = "none";
     navbarContainer.classList.remove("showOverlay");
+  });
+
+  //Footer active state. Display corresponding section when active.
+  footerNavItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      footerNavItems.forEach((item) => item.classList.remove("active"));
+      e.currentTarget.classList.add("active");
+
+      const name = e.currentTarget.getAttribute("title");
+      Object.values(containerMap).forEach((container) => {
+        container.style.display = "none";
+      });
+      // Show the clicked container
+      containerMap[name].style.display = "block";
+    });
   });
 });
