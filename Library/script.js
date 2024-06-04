86eead4fead667d97dbb5ad7ade5554030172dc8
Phong <PhongTran3@my.unt.edu>
2024-06-03 22:28:01 -0500

    Styled dialog modal

--- Library/script.js
+++ Library/script.js
@@ -1,1 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const addBtn = document.getElementById("add-btn");
+  const closeBtn = document.getElementById("close-btn");
+  const dialog = document.querySelector("dialog");
+  const myLibrary = [];
+
+  addBtn.addEventListener("click", () => {
+    dialog.showModal();
+  });
+
+  closeBtn.addEventListener("click", () => {
+    dialog.close();
+  });
+
+  function Book(title, author, numPages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.numPages = numPages;
+    this.isRead = isRead;
+  }
+
+  function addBookToLibrary() {}
+});
