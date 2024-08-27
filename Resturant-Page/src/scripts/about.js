0ce0f4ee330d0545e0d45d20c89e100b1a067372
Phong Tran <phongtran230@gmail.com>
2024-08-27 16:09:55 -0500

    Create helper function to create elements

--- Resturant-Page/src/scripts/about.js
+++ Resturant-Page/src/scripts/about.js
@@ -2,32 +2,29 @@ import "../styles/about.css";
 import beginningImg from "../assets/cold-drink-cart-business-background.jpg";
 
 
+const createElement = (tag, className, id) => {
+  const element = document.createElement(tag);
+  if(className) element.classList.add(className);
+  if(id) element.id = id;
+  return element;
+}
+
+
 export default function createAbout() {
   const content = document.getElementById("content");
   
   // Create element
   const home = document.createElement("div");
-  const ourStory = document.createElement("section");
-  const beginningContainer = document.createElement("section");
-  const beginTextContainer = document.createElement("div");
-  const beginHeader = document.createElement("div");
-  const beginningText = document.createElement("div");
-  const beginImgContainer = document.createElement("div");
-
-  const missionSection = document.createElement("section");
-  const missionTextContainer = document.createElement("div");
-  const missionText = document.createElement("div");
+  const ourStory = createElement("section", "our-story-header");
+  const beginningContainer = createElement("section", "beginning-container");
+  const beginTextContainer = createElement("div", "beginning-text-container");
+  const beginHeader = createElement("div", "beginning-header");
+  const beginningText = createElement("div", "beginning-text");
+  const beginImgContainer = createElement("div", "beginning-img-container");
+  const missionSection = createElement("section", null, "mission-statement");
+  const changeSection = createElement("section", "change");
+  const changeTextContainer = createElement("div", "change-text-container");
   
-
-  // Set classes & id
-  ourStory.classList.add("our-story-header");
-  beginningContainer.classList.add("beginning-container");
-  beginTextContainer.classList.add("beginning-text-container");
-  beginHeader.classList.add("beginning-header");
-  beginningText.classList.add("beginning-text");
-  beginImgContainer.classList.add("beginning-img-container");
-
-  missionSection.id = "mission-statement";
 
   // Set content
   ourStory.innerHTML = `<h2>Our Story</h2>`;
@@ -61,6 +58,7 @@ export default function createAbout() {
   // Combine elements
   beginTextContainer.append(beginHeader, beginningText);
   beginningContainer.append(beginTextContainer, beginImgContainer);
+
   home.append(ourStory, beginningContainer, missionSection);
   content.append(home);
 }
@@ -68,27 +66,21 @@ export default function createAbout() {
 
 {
   /* 
-      <section id="mission-statement">
-        <div class="mission-text-container">
-          <h2 class="accent">OUR MISSION</h2>
-          <div class="mission-text">
-            <p>"To enrich the human experience by serving the very best coffee, tea and goodness in every cup, every community."</p>
-          </div>
-        </div>
-      </section>
-
       <section class="change">
         <div class="change-text-container">
+
           <div class="change-header">
             <h2 class="accent">7 Leaves Cafe</h2>
             <h2>Be the change you wish to see in the world.</h2>
           </div>
+
           <div class="change-text">
             <p>
               Hanging on the wall of every 7 Leaves Cafe is a canvas of Gandhi’s famous quotation, “be the change you wish to see in the world.” It is a constant reminder and
               symbol of everything that we do – from how we create our products to the way we serve our customers, it is the very principle that guides us forward.
             </p>
           </div>
+          
         </div>
         <div class="change-img-container">
           <img src="./assets/be-the-change-wall-canvas.jpg" alt="3 people holding a large cup of 7 leaves drinks, house coffee, jasmine milk tea, and assam milk tea" />
