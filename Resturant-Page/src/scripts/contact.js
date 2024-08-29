3ef80c67e276f59c97763d4550f1c464fc8cb91b
Phong Tran <phongtran230@gmail.com>
2024-08-29 10:20:40 -0500

    Completed contact page js

--- Resturant-Page/src/scripts/contact.js
+++ Resturant-Page/src/scripts/contact.js
@@ -1,54 +1,54 @@
 import { createElement } from "./helper";
 import "../styles/contact.css";
+import contactImg from "../assets/vietnamese-coffee-splash-coffee-beans-ice-scaled.jpg"
 
 export default function createContact() {
-  const content = document.getElementById("content");
-  const home = document.createElement("div");
+    const content = document.getElementById("content");
+    const home = document.createElement("div");
 
-  // Create element
-  const contactContainer = createElement("section", "contact-container");
-  const contactDiv = createElement("div", "contact");
-  const contactHeader = createElement("div", "contact-header");
+    // Create element
+    const contactContainer = createElement("section", "contact-container");
+    const contactDiv = createElement("div", "contact");
+    const contactHeader = createElement("div", "contact-header");
+    const selectStoreDiv = createElement("div", "select-store-container");
+    const label = document.createElement("label");
+    const select = document.createElement("select");
+    const form = createElement("form", "form-container");
+    const contactImgContainer = createElement("div", "contact-img-container");
 
-  // Set content
+    // Set content
+    contactHeader.appendChild(createElement("h3", "accent", null, `CONTACT US`));
+    contactHeader.appendChild(createElement("h2", null , null, `Get in Touch`));
+    contactHeader.appendChild(createElement("p", null , null, `We value your feedback, inquiries, and suggestions.`));
+  
 
-  contactHeader.appendChild("h3", "accent", null, `CONTACT US`);
+
+    label.setAttribute("for", "stores");
+    label.textContent = "Choose a Store:";
+
+    select.name = "stores";
+    select.id = "stores";
+    select.innerHTML = `
+        <option value="Carrollton">Carrollton - 2540 Old Denton Road Ste 116</option>
+        <option value="Garland">Garland - 519 N Jupiter Rd</option>
+        <option value="Grand Prarie">Grand Prairie - 2609 W Pioneer Pkwy #100</option>
+    `
+    form.innerHTML = `
+        <input type="text" id="name" name="name" placeholder="Name" required />
+        <input type="email" id="email" name="email" placeholder="Email" required />
+        <textarea placeholder="Message" rows="4"></textarea>
+    `
+    contactImgContainer.innerHTML = `
+        <img src=${contactImg} alt="vietnamese coffee being poured and splashed into a cup" />
+    `
+    
+
+    // Combine elements
+    selectStoreDiv.append(label, select, form);
+    contactDiv.append(contactHeader, selectStoreDiv);
+
+    contactContainer.append(contactDiv, contactImgContainer);
+    home.append(contactContainer)
+    content.append(home);
 }
 
-{
-  /* 
-  <section class="contact-container">
-    <div class="contact">
-        <div class="contact-header">
-            <h3 class="accent">CONTACT US</h3>
-            <h2>Get in Touch</h2>
-            <p>We value your feedback, inquiries, and suggestions.</p>
-        </div>
-
-        <div class="store-select-container">
-            <label for="stores">Choose a Store:</label>
-            <select name="stores" id="stores">
-                <option value="Carrollton">Carrollton - 2540 Old Denton Road Ste 116</option>
-                <option value="Garland">Garland - 519 N Jupiter Rd</option>
-                <option value="Grand Prarie">Grand Prairie - 2609 W Pioneer Pkwy #100</option>
-            </select>
-
-            <form class="form-container">
-                <input type="text" id="name" name="name" placeholder="Name" required />
-                <input type="email" id="email" name="email" placeholder="Email" required />
-                <textarea placeholder="Message" rows="4"></textarea>
-            </form>
-            
-            <div class="submit-container">
-                <button class="submit-btn">Submit</button>
-            </div>
-
-        </div>
-    </div>
-
-    <div class="contact-img-container">
-        <img src="../src/assets/vietnamese-coffee-splash-coffee-beans-ice-scaled.jpg" />
-    </div>
-
-</section> */
-}
