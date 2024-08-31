409bfbc7e01d90ca745ade0dec11511acae379ce
Phong <PhongTran3@my.unt.edu>
2024-08-30 22:13:07 -0500

    Added all menu categoires item, some image size needs to be fixed

--- Resturant-Page/src/scripts/menu.js
+++ Resturant-Page/src/scripts/menu.js
@@ -1,15 +1,26 @@
 import "../styles/menu.css";
 import { createElement } from "./helper";
-import { coffeeMenu, teaMenu, milkTeaMenu, toppingMenu } from "./menuArrays";
+import { coffeeMenu, teaMenu, milkTeaMenu, addOnMenu } from "./menuArrays";
 
+function createItemCard(name, src, description) {
+  const itemCard = createElement("div", "menu-item-card");
+  const itemCardImgDiv = createElement("div", "item-card-image");
+  const itemCardText = createElement("div", "item-card-text");
+  const itemCardDesc = createElement("div", "item-card-description");
 
+  itemCardImgDiv.innerHTML = `<img src=${src} />`;
 
+  itemCardText.innerHTML = `
+    <div class="item-card-name">
+      <h3>${name}</h3>
+    </div>
+  `;
 
-function createItemCard () {
+  itemCardDesc.innerHTML = `<p>${description}</p>`;
 
+  itemCard.append(itemCardImgDiv, itemCardText, itemCardDesc);
+  return itemCard;
 }
-
-
 
 export default function createMenu() {
   const content = document.getElementById("content");
@@ -17,28 +28,72 @@ export default function createMenu() {
 
   // Create element
   const menuHeaderContainer = createElement("section", "menu-header-container");
+  const coffeesContainer = createElement("section", "menu-item-container");
+  const teaContainer = createElement("section", "menu-item-container");
+  const milkTeaContainer = createElement("section", "menu-item-container");
+  const addOnContainer = createElement("section", "menu-item-container");
 
+  const menuCategories = [
+    {
+      name: "Coffee",
+      container: coffeesContainer,
+      array: coffeeMenu,
+    },
+    {
+      name: "Tea",
+      container: teaContainer,
+      array: teaMenu,
+    },
+    {
+      name: "Milk Tea",
+      container: milkTeaContainer,
+      array: milkTeaMenu,
+    },
+    {
+      name: "Add-ons",
+      container: addOnContainer,
+      array: addOnMenu,
+    },
+  ];
 
   // Set content
   menuHeaderContainer.appendChild(createElement("h3", "accent", null, `DRINKS`));
-  menuHeaderContainer.appendChild(createElement("h2", null , null, `Flavors that Bridge Continents`));
+  menuHeaderContainer.appendChild(createElement("h2", null, null, `Flavors that Bridge Continents`));
   const menuHeaderP = document.createElement("p");
   menuHeaderP.innerHTML = `
-    <strong>OUR WAY OF DOING THINGS.</strong>
+      <strong>OUR WAY OF DOING THINGS.</strong>
       From our kitchen – we chop, peel, cook, stir, and press by hand in order to extract real flavors from raw ingredients such as fresh pandan leaves, mung bean, taro root,
       and passion fruit.
-  `
+  `;
   menuHeaderContainer.appendChild(menuHeaderP);
 
+  menuCategories.forEach((item) => {
+    const menuItemHeadContainer = createElement("div", "menu-item-header-container");
+    menuItemHeadContainer.innerHTML = `
+      <span class="menu-item-header">
+        <h3>${item.name}</h3>
+      </span>
+    `;
+
+    const menuItemGrid = createElement("grid", "menu-item-grid");
+
+    // Menu Array
+    item.array.forEach((item) => {
+      let card = createItemCard(item.name, item.imgSrc, item.description);
+      menuItemGrid.appendChild(card);
+    });
+
+    item.container.append(menuItemHeadContainer, menuItemGrid);
+  });
 
   // Combine elements
 
   home.append(menuHeaderContainer);
+  menuCategories.forEach((item) => {
+    home.appendChild(item.container);
+  });
   content.append(home);
 }
-
-
-
 
 {
   /* 
@@ -53,26 +108,21 @@ export default function createMenu() {
     <div class="menu-item-grid">
 
       <div class="menu-item-card">
-
         <div class="item-card-image">
           <img src="./assets/House-Coffee.jpg" alt="a cup of vietnamese coffee" />
         </div>
 
         <div class="item-card-text">
-
           <div class="item-card-name">
             <h3>House Coffee</h3>
           </div>
-
           <div class="item-card-description">
             <p>
               Perfected over the time span of a hundred years, Cafe Sua Da has become one of the world’s most refined and popular drinks. Using our secret brewing technique and
               the finest coffee beans in the world.
             </p>
           </div>
-
         </div>
-
       </div>
 
       <div class="menu-item-card">
--- Resturant-Page/src/scripts/menuArrays.js
+++ Resturant-Page/src/scripts/menuArrays.js
@@ -1,124 +1,132 @@
 export const coffeeMenu = [
-    {
-      name: "Vietnamese Coffee",
-      imgSrc: "./assets/Vietnamese-Coffee.jpg",
-      description: "Strong coffee with sweetened condensed milk and chilled on ice makes an unbeatable Southeast Asian treat. Even those who only take their coffee black will like this."
-    },
-    {
-      name: "House Coffee",
-      imgSrc: "./assets/House-Coffee.jpg",
-      description: "Perfected over the time span of a hundred years, Cafe Sua Da has become one of the world’s most refined and popular drinks. Using our secret brewing technique and the finest coffee beans in the world."
-    },
-    {
-      name: "Sea Cream Black Coffee",
-      imgSrc: "./assets/Sea-Cream-Black-Coffee.jpg",
-      description: "Coffee is known for many things — but refreshment is not one of them. Until now. Our Sea Cream Black Coffee is the yin and yang to coffee refinement."
-    },
-    {
-      name: "Mint Sereno",
-      imgSrc: "./assets/Mint-Serrano.jpg",
-      description: "The Mint Sereno blend is made from fresh mint, real cream and brown sugar."
-    },
-  ];
+  {
+    name: "Vietnamese Coffee",
+    imgSrc: "../src/assets/Vietnamese-Coffee.jpg",
+    description:
+      "Strong coffee with sweetened condensed milk and chilled on ice makes an unbeatable Southeast Asian treat. Even those who only take their coffee black will like this.",
+  },
+  {
+    name: "House Coffee",
+    imgSrc: "../src/assets/House-Coffee.jpg",
+    description:
+      "Perfected over the time span of a hundred years, Cafe Sua Da has become one of the world’s most refined and popular drinks. Using our secret brewing technique and the finest coffee beans in the world.",
+  },
+  {
+    name: "Sea Cream Black Coffee",
+    imgSrc: "../src/assets/Sea-Cream-Black-Coffee.jpg",
+    description: "Coffee is known for many things — but refreshment is not one of them. Until now. Our Sea Cream Black Coffee is the yin and yang to coffee refinement.",
+  },
+  {
+    name: "Mint Sereno",
+    imgSrc: "../src/assets/Mint-Serrano.jpg",
+    description: "The Mint Sereno blend is made from fresh mint, real cream and brown sugar.",
+  },
+];
 
-  export const teaMenu = [
-    {
-        name: "Sea Cream Tea Jasmine",
-        imgSrc: "./assets/Sea-Cream-Tea-Jasmine",
-        description: "Our award-winning jasmine blended with jasmine petals is grown in the high mountains and harvested every spring in the early morning before they bloom to preserve their naturally subtle sweetness."
-    },
-    {
-        name: "Summer Mint",
-        imgSrc: "./assets/Summer-Mint.jpg",
-        description: "How do you make passion fruit even more refreshing? You toss in mint leaves and a squeeze of an orange. Our Summer Mint is a special blend of our in-house green tea infused with lotus petals."
-    }, 
-    {
-        name: "Sunset Passion",
-        imgSrc: "./assets/Sunset-Passion.jpg",
-        description: "Passion Fruit is not only delicious, but also naturally high in beta carotene, potassium, Vitamin C, Lycopene, and antioxidants."
-    }, 
-    {
-        name: "Herbal Tea",
-        imgSrc: "./assets/Herbal-Tea.jpg",
-        description: "Formulated to cool and refresh the mind and body, Herbal Tea is a quenching and balanced blend of seven distinct types of herbs and teas, consisting of natural and exotic ingredients such as pandan leaves, chrysanthemum, and raw cane."
-    }, 
-    {
-        name: "Strawberry Hibiscus Tea",
-        imgSrc: "./assets/Strawberry-Hibiscus-Tea.jpg",
-        description: "Born from a deep and complex infusion of real strawberry slices, golden apples, sweet rose hips, hibiscus flowers, and passion fruit essence."
-    },
-]
-
+export const teaMenu = [
+  {
+    name: "Sea Cream Tea Jasmine",
+    imgSrc: "../src/assets/Sea-Cream-Tea-Jasmine.jpg",
+    description:
+      "Our award-winning jasmine blended with jasmine petals is grown in the high mountains and harvested every spring in the early morning before they bloom to preserve their naturally subtle sweetness.",
+  },
+  {
+    name: "Summer Mint",
+    imgSrc: "../src/assets/Summer-Mint.jpg",
+    description:
+      "How do you make passion fruit even more refreshing? You toss in mint leaves and a squeeze of an orange. Our Summer Mint is a special blend of our in-house green tea infused with lotus petals.",
+  },
+  {
+    name: "Sunset Passion",
+    imgSrc: "../src/assets/Sunset-Passion.jpg",
+    description: "Passion Fruit is not only delicious, but also naturally high in beta carotene, potassium, Vitamin C, Lycopene, and antioxidants.",
+  },
+  {
+    name: "Herbal Tea",
+    imgSrc: "../src/assets/Herbal-Tea.jpg",
+    description:
+      "Formulated to cool and refresh the mind and body, Herbal Tea is a quenching and balanced blend of seven distinct types of herbs and teas, consisting of natural and exotic ingredients such as pandan leaves, chrysanthemum, and raw cane.",
+  },
+  {
+    name: "Strawberry Hibiscus Tea",
+    imgSrc: "../src/assets/Strawberry-Hibiscus-Tea.jpg",
+    description: "Born from a deep and complex infusion of real strawberry slices, golden apples, sweet rose hips, hibiscus flowers, and passion fruit essence.",
+  },
+];
 
 export const milkTeaMenu = [
-    {
-        name: "Mung Bean Milk Tea",
-        imgSrc: "./assets/Mung-Bean-Milk-Tea",
-        description: "That Mung bean flavor you are tasting? Yes, it’s real Mung bean. Cooked, strained, and blended Mung bean has a slightly sweet and unique earthy tone that is perfect for Milk Tea infusion."
-    },
-    {
-        name: "Thai Tea",
-        imgSrc: "./assets/Thai-Tea.jpg",
-        description: "Thai Tea (also known as Thai Iced Tea) is a popular iced drink hailing from Thailand, commonly found in Thai restaurants across the U.S."
-    }, 
-    {
-        name: "Green Thai Tea",
-        imgSrc: "./assets/Green-Thai-Tea.jpg",
-        description: "A sweet and creamy milk tea made with a jasmine tea blend for a fresh floral taste."
-    }, 
-    {
-        name: "Jasmine Milk Tea",
-        imgSrc: "./assets/Jasmine-Milk-Tea.jpg",
-        description: "The Jasmine Milk Tea, perfectly curated and brewed to fit the taste of any individual, no matter the preference. Utilizing our award-winning Jasmine scented tea leaves."
-    }, 
-    {
-        name: "Taro Milk Tea",
-        imgSrc: "./assets/Taro-Milk-Tea.jpg",
-        description: "Our search for the 7 Leaves Taro Milk Tea began with a simple goal — to create a truly authentic Taro Milk Tea that captures the unique flavor and essence of real taro."
-    },
-    {
-        name: "Assam Milk Tea",
-        imgSrc: "./assets/Assam-Milk-Tea.jpg",
-        description: "Milk Teas are widely available around every corner but what makes our milk teas different? Real milk, cane sugar, and loose leaf tea. Simplicity and authenticity – simply delicious."
-    },
-    {
-        name: "Oolong Milk Tea",
-        imgSrc: "./assets/Oolong-Milk-Tea.jpg",
-        description: "Milk Teas are widely available around every corner but what makes our milk teas different? Real milk, cane sugar, and loose leaf tea. Simplicity and authenticity – simply delicious."
-    },
-    {
-        name: "Japanese Matcha and Soy",
-        imgSrc: "./assets/Japanese-Matcha-and-Soy.jpg",
-        description: "Matcha is known today as the healthiest, rarest and most premium of all tea varieties from Japan. For over 800 years, Zen Buddhist monks have been using Matcha green for meditation purposes."
-    },
-]
+  {
+    name: "Mung Bean Milk Tea",
+    imgSrc: "../src/assets/Mung-Bean-Milk-Tea.jpg",
+    description:
+      "That Mung bean flavor you are tasting? Yes, it’s real Mung bean. Cooked, strained, and blended Mung bean has a slightly sweet and unique earthy tone that is perfect for Milk Tea infusion.",
+  },
+  {
+    name: "Thai Tea",
+    imgSrc: "../src/assets/Thai-Tea.jpg",
+    description: "Thai Tea (also known as Thai Iced Tea) is a popular iced drink hailing from Thailand, commonly found in Thai restaurants across the U.S.",
+  },
+  {
+    name: "Green Thai Tea",
+    imgSrc: "../src/assets/Green-Thai-Tea.jpg",
+    description: "A sweet and creamy milk tea made with a jasmine tea blend for a fresh floral taste.",
+  },
+  {
+    name: "Jasmine Milk Tea",
+    imgSrc: "../src/assets/Jasmine-Milk-Tea.jpg",
+    description:
+      "The Jasmine Milk Tea, perfectly curated and brewed to fit the taste of any individual, no matter the preference. Utilizing our award-winning Jasmine scented tea leaves.",
+  },
+  {
+    name: "Taro Milk Tea",
+    imgSrc: "../src/assets/Taro-Milk-Tea.jpg",
+    description:
+      "Our search for the 7 Leaves Taro Milk Tea began with a simple goal — to create a truly authentic Taro Milk Tea that captures the unique flavor and essence of real taro.",
+  },
+  {
+    name: "Assam Milk Tea",
+    imgSrc: "../src/assets/Assam-Milk-Tea.jpg",
+    description:
+      "Milk Teas are widely available around every corner but what makes our milk teas different? Real milk, cane sugar, and loose leaf tea. Simplicity and authenticity – simply delicious.",
+  },
+  {
+    name: "Oolong Milk Tea",
+    imgSrc: "../src/assets/Oolong-Milk-Tea.jpg",
+    description:
+      "Milk Teas are widely available around every corner but what makes our milk teas different? Real milk, cane sugar, and loose leaf tea. Simplicity and authenticity – simply delicious.",
+  },
+  {
+    name: "Japanese Matcha and Soy",
+    imgSrc: "../src/assets/Japanese-Matcha-and-Soy.jpg",
+    description:
+      "Matcha is known today as the healthiest, rarest and most premium of all tea varieties from Japan. For over 800 years, Zen Buddhist monks have been using Matcha green for meditation purposes.",
+  },
+];
 
-
-export const toppingMenu = [
-    {
-        name: "Aloe Vera",
-        imgSrc: "./assets/Aloe-Vera",
-        description: ""
-    },
-    {
-        name: "Custard Pudding",
-        imgSrc: "./assets/Custard-Pudding.jpg",
-        description: ""
-    }, 
-    {
-        name: "Grass Jelly",
-        imgSrc: "./assets/Grass-Jelly.jpg",
-        description: ""
-    }, 
-    {
-        name: "Honey Boba",
-        imgSrc: "./assets/Honey-Boba.jpg",
-        description: ""
-    }, 
-    {
-        name: "Sea Cream",
-        imgSrc: "./assets/Sea-Cream.jpg",
-        description: ""
-    },
-    
-]
+export const addOnMenu = [
+  {
+    name: "Aloe Vera",
+    imgSrc: "../src/assets/Aloe-Vera.jpg",
+    description: "",
+  },
+  {
+    name: "Custard Pudding",
+    imgSrc: "../src/assets/Custard-Pudding.jpg",
+    description: "",
+  },
+  {
+    name: "Grass Jelly",
+    imgSrc: "../src/assets/Grass-Jelly.jpg",
+    description: "",
+  },
+  {
+    name: "Honey Boba",
+    imgSrc: "../src/assets/Honey-Boba.jpg",
+    description: "",
+  },
+  {
+    name: "Sea Cream",
+    imgSrc: "../src/assets/Sea-Cream.jpg",
+    description: "",
+  },
+];
