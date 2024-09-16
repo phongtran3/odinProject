5aea4a2681cf5a5afb118cdf830c0a7420fc5cb9
Phong <PhongTran3@my.unt.edu>
2024-09-15 19:20:51 -0500

    Added webpack codes

---
+++ Todo-List/webpack.common.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"),
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TOP Todo List",
+      template: "./src/index.html",
+    }),
+  ],
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
