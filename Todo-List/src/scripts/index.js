3be35e86f34a72af19662db52d53e82a3abb91c2
Phong Tran <phongtran230@gmail.com>
2024-10-11 14:55:23 -0500

    Added event listenrs to each task filter

--- Todo-List/src/scripts/index.js
+++ Todo-List/src/scripts/index.js
@@ -2,6 +2,10 @@ const mobileMenuBtn = document.getElementById("mobile-menu-btn");
 const overlay = document.getElementById("overlay");
 const sideBarContainer = document.getElementById("side-bar-container");
 
+const navTaskFilters = document.querySelector(".nav-task-filter");
+const filterOptions = navTaskFilters.querySelectorAll('.filter-item');
+
+
 mobileMenuBtn.addEventListener("click", () => {
     overlay.style.display = overlay.style.display === "none" ? "block" : "none";
     sideBarContainer.classList.toggle("showOverlay");
@@ -12,4 +16,15 @@ overlay.addEventListener("click", () => {
     sideBarContainer.classList.remove("showOverlay");
 });
 
+//Selecting new filter
+filterOptions.forEach ((li) => {
+    li.addEventListener("click", () => {
+        filterOptions.forEach(option => option.classList.remove('task-active'));
+        li.classList.toggle("task-active");
 
+        sideBarContainer.classList.toggle("showOverlay");
+        overlay.style.display = "none";
+
+        console.log("test");
+    })
+})
