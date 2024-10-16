1312b1a58a191c9ba4e6f5c1bc20e381345e03dc
Phong <PhongTran3@my.unt.edu>
2024-10-15 20:16:05 -0500

    Added dialog logic for adding new project

--- Todo-List/src/index.js
+++ Todo-List/src/index.js
@@ -1,8 +1,5 @@
 import { setUpDeleteDialog } from "./scripts/delete.js";
 import addEventListeners from "./scripts/eventListener.js";
 
-
 setUpDeleteDialog();
 addEventListeners();
-
-
--- Todo-List/src/scripts/eventListener.js
+++ Todo-List/src/scripts/eventListener.js
@@ -1,3 +1,4 @@
+import { setUpAddProject } from "./add.js";
 
 const mobileMenuBtn = document.getElementById("mobile-menu-btn");
 const overlay = document.getElementById("overlay");
@@ -9,57 +10,55 @@ const filterOptions = navTaskFilters.querySelectorAll(".filter-item");
 const navProjectContainer = document.getElementById("nav-project-container");
 const addProjectBtn = document.getElementById("add-project-btn");
 
-
 const addEventListeners = () => {
-    handleMobileMenuBtn();
-    handleOverlay();
-    handleFilterSelect();
-    handleNewProject();
-}
-
+  handleMobileMenuBtn();
+  handleOverlay();
+  handleFilterSelect();
+  handleNewProject();
+};
 
 const handleMobileMenuBtn = () => {
-    mobileMenuBtn.addEventListener("click", () => {
-        overlay.style.display = overlay.style.display === "none" ? "block" : "none";
-        sideBarContainer.classList.toggle("showOverlay");
-    });
-}
+  mobileMenuBtn.addEventListener("click", () => {
+    overlay.style.display = overlay.style.display === "none" ? "block" : "none";
+    sideBarContainer.classList.toggle("showOverlay");
+  });
+};
 
 const handleOverlay = () => {
-    overlay.addEventListener("click", () => {
-        overlay.style.display = "none";
-        sideBarContainer.classList.remove("showOverlay");
-      });
-}
+  overlay.addEventListener("click", () => {
+    overlay.style.display = "none";
+    sideBarContainer.classList.remove("showOverlay");
+  });
+};
 
 //Selecting new filter
 const handleFilterSelect = () => {
-    filterOptions.forEach((li) => {
-        li.addEventListener("click", () => {
-        filterOptions.forEach((option) => option.classList.remove("task-active"));
-        li.classList.toggle("task-active");
-    
-        sideBarContainer.classList.toggle("showOverlay");
-        overlay.style.display = "none";
-    
-        const mainHeader = li.querySelector("h3").textContent;
-        document.getElementById("main-header").innerHTML = mainHeader;
-    
-        console.log("test");
-        });
-    })
-}
+  filterOptions.forEach((li) => {
+    li.addEventListener("click", () => {
+      filterOptions.forEach((option) => option.classList.remove("task-active"));
+      li.classList.toggle("task-active");
 
-{/* <form class="add-project-form">
+      sideBarContainer.classList.toggle("showOverlay");
+      overlay.style.display = "none";
+
+      const mainHeader = li.querySelector("h3").textContent;
+      document.getElementById("main-header").innerHTML = mainHeader;
+
+      console.log("test");
+    });
+  });
+};
+
+{
+  /* <form class="add-project-form">
     <input type="text" minlength="3" class="new-project" />
-</form> */}
+</form> */
+}
 
 const handleNewProject = () => {
-    addProjectBtn.addEventListener("click", () => {
-        console.log("test");
-        
-    })
-}
-
+  addProjectBtn.addEventListener("click", () => {
+    setUpAddProject();
+  });
+};
 
 export default addEventListeners;
