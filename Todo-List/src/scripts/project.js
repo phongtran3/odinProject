69f86fc914480b1307401fa1e1c84d2dee8d5aec
Phong Tran <phongtran230@gmail.com>
2024-10-17 14:37:19 -0500

    Creating Project class

--- Todo-List/src/scripts/project.js
+++ Todo-List/src/scripts/project.js
@@ -1,8 +1,39 @@
 
 export class Project {
+    #tasks = [];
+    #taskID = 1;
+
     constructor(title) {
         this.title = title;
-        this.tasks = [];
+    }
+
+
+    addTask(task) {
+        if(!task) return
+        const taskWithID = { ...task, id: this.#taskID++ };
+
+        this.#tasks.push(taskWithID)
+    }
+
+    deleteTask(taskID) {
+        if(!taskID) return;
+
+        this.#tasks = this.#tasks.filter(id => id !== taskID); //Remove the task from the array
+
+        //Shift ID down
+        this.#tasks = this.#tasks.map(task => {
+            if(task.id > taskID){
+                task = {...task, id: taskID - 1};
+            }
+            
+            return task;
+        });
+
+        this.#taskID--;
+    }
+
+    getTasks() {
+        return this.#tasks;
     }
 
 }
