a5615dfa9f57eddae985cb2b8edb8b86a17ce844
Phong <PhongTran3@my.unt.edu>
2024-12-01 22:21:39 -0600

    Added webpack config and added webpack script to package json

---
+++ Weather-Page/src/scripts/screenController.js
@@ -0,0 +1,1 @@
+import { addDays, format } from "date-fns";
---
+++ Weather-Page/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "./src/index.js"),
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "TOP Weather Page",
+      template: "./src/index.html",
+    }),
+  ],
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+};
