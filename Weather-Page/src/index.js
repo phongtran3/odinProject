c230a2687941a7835b13a9e780a4dddc7142b684
Phong Tran <phongtran230@gmail.com>
2024-12-06 13:01:10 -0600

    Added and styled close button to dialog

---
+++ Weather-Page/src/index.js
@@ -0,0 +1,9 @@
+console.log("Weather Page")
+
+const overlay = document.getElementById("overlay");
+const settingBtn = document.getElementById("setting-btn");
+const settingDialog = document.getElementById("setting-dialog");
+
+
+
+
