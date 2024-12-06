c776afb17cca22ae2f9d23184aa55d6c0a3d34d4
Phong Tran <phongtran230@gmail.com>
2024-12-06 14:32:40 -0600

    Added function to open and close dialog

--- Weather-Page/src/index.js
+++ Weather-Page/src/index.js
@@ -3,7 +3,17 @@ console.log("Weather Page")
 const overlay = document.getElementById("overlay");
 const settingBtn = document.getElementById("setting-btn");
 const settingDialog = document.getElementById("setting-dialog");
+const closeBtn = document.getElementById("close-button");
 
 
+settingBtn.addEventListener("click", () => {
+    overlay.style.display = overlay.style.display === "none" ? "block" : "none";
+    settingDialog.showModal();
+})
+
+closeBtn.addEventListener("click", () => {
+    overlay.style.display = overlay.style.display === "none" ? "block" : "none";
+    settingDialog.close();
+})
 
 
