69243123a8f1fcc080c46715e5935531a845fb96
Phong Tran <phongtran230@gmail.com>
2024-12-13 12:20:43 -0600

    Added recursion method for fib

--- CS-Projects/recursion.js
+++ CS-Projects/recursion.js
@@ -17,7 +17,11 @@ const fibs = (num) => {
 
 
 const fibsRec = (num) => {
-   
+   if(num === 2) return [0,1];
+   let array = fibsRec(num - 1);
+
+   array.push(array[array.length - 2] + array[array.length - 1]);
+   return array;
 }
 
 
