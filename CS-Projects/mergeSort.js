9575a9a3ec1d0bc93dacf3d70142de045b6c9bba
Phong Tran <phongtran230@gmail.com>
2024-12-13 16:25:01 -0600

    Working on merge sort function

--- CS-Projects/mergeSort.js
+++ CS-Projects/mergeSort.js
@@ -1,7 +1,24 @@
-const mergeSort = (arr) => {
-    if(arr.length === 1) return arr;
+const merge = (left, right) => {
+    let l = 0;
+    let r = 0;
+    let array = [];
 
-    
 
-
+    return array;
 }
+
+
+
+const mergeSort = (arr) => {
+    if(arr.length <= 1) return arr;
+
+    let mid = Math.floor(arr.length / 2);
+    
+    let left = mergeSort(arr.slice(0, mid));
+    let right = mergeSort(arr.slice(mid));
+    console.log("Left:", left, "Right:", right);
+
+    return merge(left, right);
+}
+
+mergeSort([3, 2, 1, 13, 8, 5, 0, 1]);
