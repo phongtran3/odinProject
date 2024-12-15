e4b4a6787ba87db2499c2491440b15d2ab6b1441
Phong <PhongTran3@my.unt.edu>
2024-12-14 21:22:14 -0600

    completed merge sort function

--- CS-Projects/mergeSort.js
+++ CS-Projects/mergeSort.js
@@ -1,24 +1,30 @@
 const merge = (left, right) => {
-    let l = 0;
-    let r = 0;
-    let array = [];
+	let array = [];
+	let l = 0;
+	let r = 0;
 
+	while (l < left.length && r < right.length) {
+		if (left[l] < right[r]) {
+			array.push(left[l]);
+			l++;
+		} else {
+			array.push(right[r]);
+			r++;
+		}
+	}
+	array.push(...left.slice(l), ...right.slice(r));
 
-    return array;
-}
-
-
+	return array;
+};
 
 const mergeSort = (arr) => {
-    if(arr.length <= 1) return arr;
+	if (arr.length <= 1) return arr;
 
-    let mid = Math.floor(arr.length / 2);
-    
-    let left = mergeSort(arr.slice(0, mid));
-    let right = mergeSort(arr.slice(mid));
-    console.log("Left:", left, "Right:", right);
+	let mid = Math.floor(arr.length / 2);
+	let left = mergeSort(arr.slice(0, mid));
+	let right = mergeSort(arr.slice(mid));
 
-    return merge(left, right);
-}
+	return merge(left, right);
+};
 
 mergeSort([3, 2, 1, 13, 8, 5, 0, 1]);
