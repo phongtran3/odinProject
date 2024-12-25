1b3ba52bc09a8f53945e879a89e6412b77614bae
Phong <PhongTran3@my.unt.edu>
2024-12-25 17:26:38 -0600

    Added buildTree and insert function

--- CS-Projects/binarySearchTree.js
+++ CS-Projects/binarySearchTree.js
@@ -8,15 +8,48 @@ class Node {
 
 class Tree {
 	constructor(array) {
+		if (array.length === 0) throw new Error("Empty Array");
+
 		this.array = [...new Set(array.sort((a, b) => a - b))];
 		this.root = this.buildTree(this.array);
 	}
 
-	buildTree = (array, start = 0, end = array.length - 1) => {
-		if (array.length === 0) throw new Error("Empty Array");
+	prettyPrint = (node, prefix = "", isLeft = true) => {
+		if (node === null) {
+			return;
+		}
+		if (node.right !== null) {
+			this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+		}
+		console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+		if (node.left !== null) {
+			this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+		}
 	};
 
-	insert = (value) => {};
+	buildTree = (array, start = 0, end = array.length - 1) => {
+		if (start > end) return null;
+
+		let mid = Math.floor((start + end) / 2);
+		let root = new Node(array[mid]);
+
+		root.left = this.buildTree(array, start, mid - 1);
+		root.right = this.buildTree(array, mid + 1, end);
+		return root;
+	};
+
+	insert = (value, current = this.root) => {
+		if (current === null) return new Node(value);
+		if (current.value === value) return current;
+
+		if (value < current.value) {
+			current.left = this.insert(value, current.left);
+		} else {
+			current.right = this.insert(value, current.right);
+		}
+
+		return current;
+	};
 
 	deleteItem = (value) => {};
 
@@ -30,3 +63,8 @@ class Tree {
 	isBalance = () => {};
 	rebalance = () => {};
 }
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+console.log(tree.root);
+tree.prettyPrint(tree.root);
