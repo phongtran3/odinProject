2707b2334ffe9f1e4d42687418e606e5a67a9d64
Phong <PhongTran3@my.unt.edu>
2025-01-01 20:40:51 -0600

    Added last test for test practice project

--- CS-Projects/jestPractice.js
+++ CS-Projects/jestPractice.js
@@ -1,6 +1,6 @@
-export const sum = (a, b) => {
-	return a + b;
-};
+// export const sum = (a, b) => {
+// 	return a + b;
+// };
 
 export const capitalize = (word) => {
 	return word[0].toUpperCase() + word.slice(1);
@@ -29,8 +29,33 @@ export class Calculator {
 	};
 }
 
-export const caesarCipher = (string, factor) => {
-	const isPunctuation = (string) => {
-		const punctuationRegex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+export const caesarCipher = (string, shift) => {
+	const punctuationMarks = new Set([",", ".", "!", "?", ";", ":", "-", "(", ")", "[", "]", "{", "}", '"', "'", "...", "@", "#", "$", "%", "^", "&", "*", "+", "=", "_", "~"]);
+
+	const isPunctuation = (char) => {
+		return punctuationMarks.has(char);
 	};
+
+	let result = "";
+
+	string.split("").forEach((char) => {
+		if (isPunctuation(char) || char === " ") {
+			result += char;
+		} else {
+			let start = char === char.toUpperCase() ? 65 : 97;
+			result += String.fromCharCode(((char.charCodeAt(0) - start + shift) % 26) + start);
+		}
+	});
+
+	return result;
+};
+
+export const analyzeArray = (array) => {
+	let object = {};
+	object.average = array.reduce((a, b) => a + b) / array.length;
+	object.min = Math.min(...array);
+	object.max = Math.max(...array);
+	object.length = array.length;
+
+	return object;
 };
--- CS-Projects/jestPractice.test.js
+++ CS-Projects/jestPractice.test.js
@@ -1,4 +1,4 @@
-import { sum, capitalize, reverse, Calculator, caesarCipher } from "./jestPractice";
+import { sum, capitalize, reverse, Calculator, caesarCipher, analyzeArray } from "./jestPractice";
 
 // it("adds 1 + 2 to equal 3", () => {
 // 	expect(sum(1, 2)).toBe(3);
@@ -44,4 +44,21 @@ it("Caesar Cipher", () => {
 	expect(caesarCipher("xyz", 3)).toBe("abc");
 	expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
 	expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
+});
+
+//TEST 5
+it("Analyze Array", () => {
+	expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+		average: 4,
+		min: 1,
+		max: 8,
+		length: 6,
+	});
+
+	expect(analyzeArray([2, 16, 6, 8, 4, 12])).toStrictEqual({
+		average: 8,
+		min: 2,
+		max: 16,
+		length: 6,
+	});
 });
