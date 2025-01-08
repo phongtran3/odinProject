64aaca85f141295584e1979b126f31474944f7d0
Phong <PhongTran3@my.unt.edu>
2025-01-07 23:05:11 -0600

    Working on gameboard class

--- Battleship/src/scripts/models/gameboard.js
+++ Battleship/src/scripts/models/gameboard.js
@@ -1,1 +1,17 @@
-export class Gameboard {}
+import { Ship } from "./ship";
+
+export class Gameboard {
+	constructor() {
+		this.board = Array.from({ length: 10 }, () => Array.from({ length: 10 }, () => ({ ship: null, hit: false })));
+		this.fleet = []; //{ship: "", hit: int}
+		this.shipSunk = 0;
+	}
+
+	receiveAttack = (name, [x, y]) => {};
+
+	placeShip = (name, startCoordinate, orientation) => {};
+
+	isFullySunk = () => {
+		return this.shipSunk === this.fleet.length;
+	};
+}
